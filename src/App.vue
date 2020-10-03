6d6cdc65f7f5aa4f100e3f170ae316b0d92419b9
sam <247765564@qq.com>
2020-10-03 21:54:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div>vue2 template</div>
+    <div>for github</div>
+    <div>dev/1.0.0</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
